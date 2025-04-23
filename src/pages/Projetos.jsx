434eeafadb2dd43ee618d29e03b117cd6438d3fd
Kaik Aciole <kaikaciole1.0@gmail.com>
2025-04-22 21:21:20 -0300

    feat: adiciona componentes usados nas rotas

---
+++ src/pages/Projetos.jsx
@@ -0,0 +1,10 @@
+function Projetos(){
+    return(
+        <div>
+            <h1>Projetos</h1>
+            <p>Conteúdo da Página</p>
+        </div>
+    )
+}
+
+export default Projetos;
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,10 @@
+function Sobre(){
+    return(
+        <div>
+            <h1>Sobre</h1>
+            <p>Conteúdo da Página</p>
+        </div>
+    )
+}
+
+export default Sobre;
